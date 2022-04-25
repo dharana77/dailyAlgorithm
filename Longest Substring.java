1effd16121546578e532fc54a72319a647e9ce42
dharana <dharana7723@gmail.com>
2022-04-26 04:16:41 +0900

    fix: 시간 통과 및 O(n) 알고리즘으로 변경

--- Longest Substring.java
+++ Longest Substring.java
@@ -1,47 +1,48 @@
-import java.util.*;
+    import java.util.*;
 
-//시간 초과 경우
-// https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
-class Solution {
-    private int MAX_CHAR = 256;
-    
-    public int lengthOfLongestSubstring(String s) {
-        int answer = 0 ;
-        for(int i=0; i<s.length(); i++){
-            for(int j=i+1; j<= s.length(); j++){
-                String target = s.substring(i, j);
-                
+    class Solution {
+        private int MAX_CHAR = 256;
+
+        public int lengthOfLongestSubstring(String s) {
+            int answer = 0 ;
+            String target = "";
+            for(int i=0; i<s.length(); i++){
+                target += s.charAt(i);
                 if(isUnique(target)){
+                    // System.out.println(target);
                     if(answer<target.length()){
+                        
                         answer = target.length();
                     }
+                }else{
+                    target = target.substring( target.indexOf(s.charAt(i)) + 1);  
+                    // System.out.println(target);
                 }
-                
-            }
-        }
-        return answer;
-    }
-    
-    public boolean isUnique(String target){
-        // System.out.println(target);
-        boolean[] chars = new boolean[MAX_CHAR];
-        Arrays.fill(chars, false);
-        
-        if(target.length() > MAX_CHAR){
-            return false;
-        }
-        
-        for(int i=0; i<target.length(); i++){
-            int index = (int)target.charAt(i);
             
-            if(chars[index]==true){
+            }
+            return answer;
+        }
+
+        public boolean isUnique(String target){
+            // System.out.println(target);
+            boolean[] chars = new boolean[MAX_CHAR];
+            Arrays.fill(chars, false);
+
+            if(target.length() > MAX_CHAR){
                 return false;
             }
-            if(chars[index]==false){
-                chars[index]=true;
+
+            for(int i=0; i<target.length(); i++){
+                int index = (int)target.charAt(i);
+
+                if(chars[index]==true){
+                    return false;
+                }
+                if(chars[index]==false){
+                    chars[index]=true;
+                }
+
             }
-            
+            return true;
         }
-        return true;
     }
-}
