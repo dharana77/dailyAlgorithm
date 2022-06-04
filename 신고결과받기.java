ec9327b33303255ebb3716d4f149eff24db8101d
jinyoung <dharana7723@gmail.com>
2022-06-04 23:16:28 +0900

    feat: 신고결과받기.java hashmap test

---
+++ 신고결과받기.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        HashMap<String, String> map = new HashMap<>();
+        return answer;
+    }
+}
