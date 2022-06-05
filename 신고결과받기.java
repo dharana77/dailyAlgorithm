97a8323d98b8b57e3a67f3379b7d0971e7273a1d
dharana <dharana7723@gmail.com>
2022-06-05 23:52:51 +0900

    feat: 수정전 신고결과 자바

--- 신고결과받기.java
+++ 신고결과받기.java
@@ -1,9 +1,49 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
-        HashMap<String, String> map = new HashMap<>();
+        
+        HashMap<String, List<String>> reportedListById = new HashMap<String, List<String>>();
+        
+        for(int i=0; i<id_list.length; i++){
+            // System.out.println(id_list[i]);
+            List<String> reported_list = new ArrayList<String>();
+            
+            for(int j=0; j<report.length; j++){
+                String reporter = report[j].split(" ")[0];
+                String reported = report[j].split(" ")[1];
+                if(reporter == id_list[i]){
+                    reported_list.add(reported);
+                }
+            }
+            Set<String> reported = new HashSet<String>(reported_list);
+            List<String> set_reported = new ArrayList<String>(reported);
+            reportedListById.put(id_list[i], set_reported);
+        }
+        HashMap<String, Integer> reportedCountById = new HashMap<String, Integer>();
+        for(int i=0; i<id_list.length; i++){
+            reportedCountById.put(id_list[i], 0);
+        }
+        for(int i=0; i<id_list.length; i++){
+            List<String>reportedList = reportedListById.get(id_list[i]);
+            for(int j=0; j<reportedList.size(); j++){
+                Integer targetValue = reportedCountById.get(reportedList.get(j));
+                reportedCountById.put(reportedList.get(j), targetValue+1);
+            }
+        }
+        ArrayList<Integer> answerList = new ArrayList<Integer>();
+        for(int i=0; i<id_list.length; i++){
+            int ct = 0;
+            List<String> localReportedList = reportedListById.get(id_list[i]);
+            for(int j=0; j<localReportedList.size(); j++){
+                if(reportedCountById.get(localReportedList.get(j))>=k){
+                    ct+=1;
+                }
+            }
+            answerList.add(ct);
+        }
+        int[] answer = new int[answerList.size()];
+        answer = answerList.toArray(new int[0]);
         return answer;
     }
 }
