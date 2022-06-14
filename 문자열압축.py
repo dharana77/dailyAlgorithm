610ad27ff3b4fdd485e904246bf82fda654c6abb
dharana <dharana7723@gmail.com>
2022-06-15 05:10:06 +0900

    feat: 문자열압축 풀이

--- 문자열압축.py
+++ 문자열압축.py
@@ -1,20 +1,22 @@
 def solution(s):
     answer = len(s)
-    for ln in range(2,len(s)):
+    for ln in range(1,len(s)):
         temp_string = ""
         start = 0
         same_string_count = 1
-        for start in range(0,len(s),ln):
-            if start+ln*2 <= len(s):
-                if s[start:start+ln] == s[start+ln:start+ln*2]:
-                    same_string_count+=1
-                else:
-                    if same_string_count !=1:
-                        temp_string+=s[start:start+ln]+str(same_string_count)
-                    else:
-                        temp_string+=s[start:start+ln]
-                    same_string_count = 1
+        while (start+2*ln) <= len(s):
+            if s[start:start+ln] == s[start+ln:start+2*ln]:
+                same_string_count+=1
             else:
-                temp_string+=s[start:start+ln]    
-        print(temp_string)
+                temp_string+=s[start:start+ln]
+                if same_string_count != 1:
+                    temp_string += str(same_string_count)
+                same_string_count=1
+            start+=ln
+        temp_string += s[start:]
+        if same_string_count!=1:
+            temp_string+= str(same_string_count)
+        # print(start, ln, temp_string)
+        if len(temp_string)<answer:
+            answer = len(temp_string)
     return answer
