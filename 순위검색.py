8bcef9d53415b8677d134fd2e7b42d911e1b1cfe
dharana <dharana7723@gmail.com>
2022-06-28 22:12:52 +0900

    feat: 순위검색 속도 수정

--- 순위검색.py
+++ 순위검색.py
@@ -1,9 +1,34 @@
+from collections import defaultdict
+from bisect import bisect_left
+
 def solution(info, query):
     answer = []
     #cpp, java, python
     #backend, frontend
     #junior, senior
     #chicken, pizza
+    hash_map = defaultdict(list)
+    for inf in info:
+        inf = inf.split(" ")
+        language = inf[0]
+        position = inf[1]
+        title = inf[2]
+        soul = inf[3]
+        score = inf[4]
+        lan = ["-"] if language == "-" else ["-", language]
+        pos = ["-"] if position == "-" else ["-", position]
+        tit = ["-"] if title =="-" else ["-", title]
+        so = ["-"] if soul == "-" else ["-", soul]
+        
+        for a in lan:
+            for b in pos:
+                for c in tit:
+                    for d in so:
+                        key = a + b + c + d
+                        hash_map[key].append(int(score))
+    for key in hash_map:
+        hash_map[key] = sorted(hash_map[key])
+        
     for q in query:
         q = q.split(" ")
         # print(q, len(q))
@@ -11,37 +36,19 @@ def solution(info, query):
         position = q[2]
         title = q[4]
         soul = q[6]
-        score = q[7]
+        score = int(q[7])
         ct = 0
-        print("q:", q)
+        # print("q:", q)
         
-        for inf in info:
-            inf = inf.split(" ")
-            # print(inf, len(inf))
-            inf_language = inf[0]
-            inf_position = inf[1]
-            inf_title = inf[2]
-            inf_soul = inf[3]
-            inf_score = inf[4]
-            con = True
-            if language != '-':
-                if language!=inf_language:
-                    con = False
-                    continue 
-            if position != '-':
-                if inf_position!=position:
-                    con = False
-                    continue
-            if title != '-':
-                if inf_title != title:
-                    con = False
-                    continue
-            if soul!='-':
-                if inf_soul != soul:
-                    con = False
-                    continue
-            if int(score)<=int(inf_score):
-                ct+=1
-                # print(inf, ct)
+        ct = 0
+        key = language + position + title + soul
+        target = hash_map[key]
+        # print(target)
+        ln = len(target)
+        # print(target)
+        x = bisect_left(target, int(score))
+        # if ln-x>0:
+        #     print("플러스", ln-x, "x", x)
+        ct += (ln - x)
         answer.append(ct) 
     return answer
