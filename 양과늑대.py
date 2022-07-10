f7fae5bfaef0c92ed4a1abc27fa79e1e733f83f9
dharana <dharana7723@gmail.com>
2022-07-10 22:35:50 +0900

    fix: 양과 늑대 코드 수정 테스트

--- 양과늑대.py
+++ 양과늑대.py
@@ -1,35 +1,37 @@
+#10:26 ~
+from collections import defaultdict, deque
+from copy import deepcopy
 
+is_wolf = list()
+num_to_edges = defaultdict(list)
+max_sheep = 0
+
+
+def find_max_recursive(current_loc, nsheep, nwolf, can_go):
+    global max_sheep
+    if is_wolf[current_loc]:
+        nwolf+=1
+    else:
+        nsheep+=1
+        max_sheep = max(max_sheep, nsheep)
+        
+    if nsheep <= nwolf:
+        return
+    
+    can_go.extend(num_to_edges[current_loc])
+    for next_loc in can_go:
+        find_max_recursive(next_loc, nsheep, nwolf, can_go=[loc for loc in can_go if loc!= next_loc])
+                           
+                           
 def solution(info, edges):
-    answer = 0
-    node = array_to_bst(info, 0)
-    # print(node.data)]
-    preorder(node)
-    return answer
-
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-def preorder(node):
-    print(node.data)
-    if node.left!= None:
-        preorder(node.left)
-    if node.right!=None:
-        preorder(node.right)
+    global is_wolf, num_to_edges, max_sheep
+    is_wolf = info
     
-def array_to_bst(nums,head):
-    node = Node(nums[head])
+    for edge_from, edge_to in edges:
+        num_to_edges[edge_from].append(edge_to)
+    print(num_to_edges)
+    for i, v in num_to_edges.items():
+        print(i,":",v)
+    find_max_recursive(0, 0, 0, [])
     
-    left = pow(2, head)
-    right = pow(2, head)+1
-    if left<len(nums):
-        node.left = Node(left)
-        array_to_bst(nums, left)
-    if right<len(nums):
-        node.right = Node(right)
-        array_to_bst(nums, right)
-
-    return node
+    return max_sheep
