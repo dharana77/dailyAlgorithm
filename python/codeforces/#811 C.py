45dd4ba9f1ab9f545bddb99e91c8bd8b6380e5f4
dharana <dharana7723@gmail.com>
2022-08-04 02:17:42 +0900

    fix:#811 C 수정

--- python/codeforces/#811 C.py
+++ python/codeforces/#811 C.py
@@ -1,83 +1,17 @@
 from collections import defaultdict
  
 t = int(input())
-#수정하기 ㅋㅋㅋㅋ 
+
 for i in range(t):
     n = int(input())
- 
-    if n < 10:
-        print(n)
-        continue
- 
-    if n == 10:
-        print(19)
-    elif n == 11:
-        print(29)
-    elif n == 12:
-        print(39)
-    elif n == 13:
-        print(49)
-    elif n == 14:
-        print(59)
-    elif n== 15:
-        print(69)
-    elif n == 16:
-        print(79)
-    elif n == 17:
-        print(89)
-    elif n == 18:
-        print(189)
-    elif n == 19:
-        print(289)
-    elif n == 20:
-        print(389)
-    elif n == 21:
-        print(489)
-    elif n == 22:
-        print(589)
-    elif n == 23:
-        print(689)
-    elif n == 24:
-        print(789)
-    elif n == 25:
-        print(1789)
-    elif n == 26:
-        print(2789)
-    elif n == 27:
-        print(3789)
-    elif n == 28:
-        print(4789)
-    elif n == 29:
-        print(5789)
-    elif n == 30:
-        print(6789)
-    elif n == 31:
-        print(16789)
-    elif n == 32:
-        print(26789)
-    elif n == 33:
-        print(36789)
-    elif n == 34:
-        print(46789)
-    elif n == 35:
-        print(56789)
-    elif n == 36:
-        print(156789)
-    elif n == 37:
-        print(256789)
-    elif n == 38:
-        print(356789)
-    elif n == 39:
-        print(456789)
-    elif n == 40:
-        print(1456789)
-    elif n == 41:
-        print(2456789)
-    elif n == 42:
-        print(3456789)
-    elif n == 43:
-        print(13456789)
-    elif n == 44:
-        print(23456789)
-    elif n == 45:
-        print(123456789)
+    #greedy로 풀면 됌
+    
+    answer = ""
+    for j in range(9,0,-1):
+        # print("j:" + str(j))
+        if n>=j:
+            n-=j
+            answer = str(j) + answer
+    
+    print(answer)
+    
