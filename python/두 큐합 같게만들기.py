e383b2811e93e2ff6fd8e65218d8e293f4633221
dharana <dharana7723@gmail.com>
2022-08-20 22:36:05 +0900

    feat: 두 큐합 같게 만들기 .py 시간초과 해결

--- python/두 큐합 같게만들기.py
+++ python/두 큐합 같게만들기.py
@@ -1,10 +1,19 @@
-## 시간초과
+## list를 디큐로 변경해서 빨라졌으나 변경된 시간복잡도 계산과
+## 목표값을 target으로 잡아 한쪽에서만 빼고 변경하는 부분
+## 왜 mx_len * 2 + 2 보다 클때인지
+from collections import deque
+[1,1,10,1,] 1, 
+[2,2,1,2,], 3 +3 
+
+[]
+
 def solution(queue1, queue2):
     answer = 0
     tot = 0
     mx = 0
     a_tot = 0
     b_tot = 0 
+    mx_len = max(len(queue1), len(queue2))
     
     for a in queue1:
         tot += a
@@ -20,23 +29,28 @@ def solution(queue1, queue2):
     div_target = tot/2
     if mx > div_target or div_target %1 !=0:
         answer = -1
+        return answer
     
-    if answer != -1:
-        while a_tot != b_tot:
-            if a_tot > b_tot:
-                target = queue1[0]
-                queue1 = queue1[1:]
-                queue2.append(target)
-                a_tot -= target
-                b_tot += target
-                
-            elif b_tot > a_tot:
-                target = queue2[0]
-                queue2 = queue2[1:]
-                queue1.append(target)
-                a_tot += target
-                b_tot -= target
-                
-            answer+=1
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    
+    while a_tot != b_tot:
+        if a_tot > b_tot:
+            target = queue1.popleft()
+            queue2.append(target)
+            a_tot -= target
+            b_tot += target
+
+        elif b_tot > a_tot:
+            target = queue2.popleft()
+            queue1.append(target)
+            a_tot += target
+            b_tot -= target
+
+        elif a_tot == b_tot:
+            return answer
+        answer+=1
+        if answer > mx_len * 2 + 2: #아직 이해 못한부분
+            return -1
     
     return answer
