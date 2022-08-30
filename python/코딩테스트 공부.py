4bca5bd90696cf4c1eb93d4235fdf13f444fd4ac
dharana <dharana7723@gmail.com>
2022-08-30 22:53:04 +0900

    feat: 코딩테스트 공부.py 푸는 중

---
+++ python/코딩테스트 공부.py
@@ -0,0 +1,11 @@
+def solution(alp, cop, problems):
+    answer = 0
+    
+    for pr in problems:
+        algo = pr[0]
+        coding = pr[1]
+        up_algo = pr[2]
+        up_coding = pr[3]
+        cost = pr[4]
+        
+    return answer
